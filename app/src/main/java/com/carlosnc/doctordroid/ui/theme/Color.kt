1b48442d2e4d2e953f4a3f13a69fe174ea96856c
Carlos Costa <carllos.nc@gmail.com>
2026-02-18 01:57:16 -0300

    Update theme colors: Replace purple/pink palette with blue and cyan variants

--- app/src/main/java/com/carlosnc/doctordroid/ui/theme/Color.kt
+++ app/src/main/java/com/carlosnc/doctordroid/ui/theme/Color.kt
@@ -2,10 +2,22 @@ package com.carlosnc.doctordroid.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Blue50  = Color(0xFFEFF6FF)
+val Blue100 = Color(0xFFDBEAFE)
+val Blue200 = Color(0xFFBFDFFE)
+val Blue300 = Color(0xFF93C5FD)
+val Blue400 = Color(0xFF60A5FA)
+val Blue500 = Color(0xFF3B82F6)
+val Blue600 = Color(0xFF2563EB)
+val Blue700 = Color(0xFF1D4ED8)
+val Blue800 = Color(0xFF1E40AF)
+val Blue900 = Color(0xFF1E3A8A)
+val Blue950 = Color(0xFF172554)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Blue80 = Color(0xFFD0E4FF)
+val BlueGrey80 = Color(0xFFC2CCD0)
+val Cyan80 = Color(0xFFB8EBEF)
+
+val Blue40 = Color(0xFF005FAF)
+val BlueGrey40 = Color(0xFF5B6771)
+val Cyan40 = Color(0xFF006875)
